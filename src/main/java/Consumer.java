6be53f6563b5668ff6675abcb652afc77571d37c
incompr <88598531+incompr@users.noreply.github.com>
2023-03-05 00:48:09 +0300

    initial

---
+++ src/main/java/Consumer.java
@@ -0,0 +1,31 @@
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Consumer {
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://reqres.in/api/users/2";
+        String response = restTemplate.getForObject(url, String.class);
+
+        System.out.println(response);
+
+        Map<String, String> jsonToSend = new HashMap<>();
+        jsonToSend.put("name", "TestName");
+        jsonToSend.put("job", "TestJob");
+
+        HttpEntity<Map<String, String>> request  = new HttpEntity<>(jsonToSend);
+
+        String url2 = "https://reqres.in/api/users";
+        String response2 = restTemplate.postForObject(url2, request, String.class);
+
+        System.out.println(response2);
+
+
+    }
+
+
+
+}
